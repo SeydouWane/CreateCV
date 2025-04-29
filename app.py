5a027d6c0e9e1085c52a419885af6bd05e67a13c
MacBook Air <macbookair@MacBook-Air.local>
2025-04-29 12:58:26 +0000

    Ajout 3eme partie

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ def step1a():
             languages.append(language_info)
 
         session['languages'] = languages
-        return redirect('/step2')  
-
+        return redirect('/step2')
     return render_template('form_step1a.html')
+
 
 @app.route('/step2', methods=['GET', 'POST'])
 def step2():
@@ -93,6 +93,54 @@ def step2():
     return render_template('form_step2.html')
 
 
+@app.route('/step3', methods=['GET', 'POST'])
+def step3():
+    if request.method == 'POST':
+        skills = request.form.getlist('skills[]')
+        session['skills'] = skills
+        return redirect('/step4')
+    return render_template('form_step3.html')
+
+
+@app.route('/step4', methods=['GET', 'POST'])
+def step4():
+    if request.method == 'POST':
+        experiences = []
+
+        titles = request.form.getlist('title[]')
+        companies = request.form.getlist('company[]')
+        locations = request.form.getlist('location[]')
+        types = request.form.getlist('type[]')
+        start_dates = request.form.getlist('start_date[]')
+        end_dates = request.form.getlist('end_date[]')
+        currents = request.form.getlist('current[]')
+        descriptions = request.form.getlist('description[]')
+        ref_names = request.form.getlist('ref_name[]')
+        ref_contacts = request.form.getlist('ref_contact[]')
+
+        for i in range(len(titles)):
+            # On ignore les lignes vides
+            if not titles[i].strip() and not companies[i].strip():
+                continue
+
+            experiences.append({
+                'title': titles[i],
+                'company': companies[i],
+                'location': locations[i],
+                'type': types[i],
+                'start_date': start_dates[i],
+                'end_date': end_dates[i] if end_dates[i] else None,
+                'current': True if i < len(currents) else False,
+                'description': descriptions[i],
+                'ref_name': ref_names[i],
+                'ref_contact': ref_contacts[i]
+            })
+
+        session['experiences'] = experiences
+        return redirect('/review')
+    return render_template('form_step4.html')
+
+
 @app.route('/review')
 def review():
     return render_template('review.html', data=session)
@@ -106,14 +154,59 @@ def generate_cv():
     response.headers['Content-Disposition'] = 'attachment; filename=cv.pdf'
     return response
 
+from flask import render_template, session, make_response
+from deep_translator import GoogleTranslator
+import pdfkit
 
-@app.route('/step3', methods=['GET', 'POST'])
-def step3():
-    if request.method == 'POST':
-        skills = request.form.getlist('skills[]')
-        session['skills'] = skills
-        return redirect('/review')
-    return render_template('form_step3.html')
+@app.route('/generate_cv_en')
+def generate_cv_en():
+    # Cloner les données depuis la session
+    data = session.copy()
+    translator = GoogleTranslator(source='fr', target='en')
+
+    def t(text):
+        return translator.translate(text) if text else ''
+
+    # Traduction des compétences
+    if 'skills' in data:
+        data['skills'] = [t(s) for s in data['skills']]
+
+    # Traduction de l’éducation
+    if 'education' in data:
+        for edu in data['education']:
+            edu['degree'] = t(edu.get('degree'))
+            edu['school'] = t(edu.get('school'))
+            edu['courses'] = [t(c) for c in edu.get('courses', [])]
+
+    # Traduction des expériences professionnelles
+    if 'experiences' in data:
+        for exp in data['experiences']:
+            exp['title'] = t(exp.get('title'))
+            exp['company'] = t(exp.get('company'))
+            exp['location'] = t(exp.get('location'))
+            exp['description'] = t(exp.get('description'))
+            exp['type'] = t(exp.get('type'))
+            exp['ref_name'] = t(exp.get('ref_name'))
+            exp['ref_contact'] = exp.get('ref_contact')  # Ne pas traduire email/téléphone
+
+    # Traduction des langues
+    if 'languages' in data:
+        for lang in data['languages']:
+            lang['name'] = t(lang.get('name'))
+
+    # Traduction partielle des infos perso
+    if 'personal_info' in data:
+        data['personal_info']['name'] = data['personal_info'].get('name')
+        data['personal_info']['linkedin'] = data['personal_info'].get('linkedin')
+
+    # Générer le PDF à partir du template anglais
+    rendered = render_template('cv_template_en.html', data=data)
+    pdf = pdfkit.from_string(rendered, False)
+
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'attachment; filename=cv_en.pdf'
+    return response
 
 
 if __name__ == '__main__':
